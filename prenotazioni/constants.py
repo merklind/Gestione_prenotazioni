9aa9921a88d93e8208a3981c5342e5ae8a83ebae
marcomelis <marco.melis@mail.polimi.it>
2020-03-28 13:05:02 +0100

    update xlsx name file

--- prenotazioni/constants.py
+++ prenotazioni/constants.py
@@ -1,7 +1,7 @@
 # CONTSTANTS
 
-MASTER_FILE = 'master entrate e dettaglio check in-out.xlsx'
-DETTAGLIO_FILE = 'dettaglio prezzo per notte.xlsx'
+MASTER_FILE = '001 master entrate e dettaglio check in-out.xlsx'
+DETTAGLIO_FILE = '002 dettaglio prezzo per notte.xlsx'
 COLUMN_FILE = 'resource/column_label.json'
 APARTMENT_FILE ='resource/apartment.json'
 MAX_ROW_FILE = 'resource/max_row_dettaglio_prezzo.json'
