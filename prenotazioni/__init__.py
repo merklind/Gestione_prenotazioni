9618eb5adde92f651240b1c2c367e42aef79ce56
marcomelis <marco.melis@mail.polimi.it>
2020-03-28 13:05:55 +0100

    commmented generate_json function

--- prenotazioni/__init__.py
+++ prenotazioni/__init__.py
@@ -2,6 +2,6 @@ from prenotazioni.constants import MASTER_FILE, COLUMN_FILE
 
 from prenotazioni.utils.json_utils import generate_json
 
-generate_json(MASTER_FILE, COLUMN_FILE)
+# generate_json(MASTER_FILE, COLUMN_FILE)
 
 
