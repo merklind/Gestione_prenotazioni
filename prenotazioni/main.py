481db810c7c448d69cc1ec94b151af14a7de17fc
marcomelis <marco.melis@mail.polimi.it>
2020-07-31 19:17:37 +0200

    Loop choose modality

--- prenotazioni/main.py
+++ prenotazioni/main.py
@@ -1,8 +1,10 @@
 opt = int(input(f'Vuoi calcolare:\n1) Solo le prenotazioni future\n2) Tutte le prenotazioni\n'))
 
-if opt == 1:
-    import prenotazioni.main_future_reservation
-elif opt == 2:
-    import prenotazioni.main_all_reservation
-else:
-    print(f'Hai sbagliato a digitare')
+while opt != (1, 2):
+    if opt == 1:
+        import prenotazioni.main_future_reservation
+    elif opt == 2:
+        import prenotazioni.main_all_reservation
+    else:
+        print(f'Hai sbagliato a digitare\n')
+        opt = int(input(f'Vuoi calcolare:\n1) Solo le prenotazioni future\n2) Tutte le prenotazioni\n'))
