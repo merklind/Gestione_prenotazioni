d1d8de440dafaee2c7b63bbb509a51e55fd3ae36
Marco Melis <merklind@libero.it>
2022-09-25 15:49:08 +0200

    Compute max_col instead of hardcode it

--- prenotazioni/style_worksheet/style_excel.py
+++ prenotazioni/style_worksheet/style_excel.py
@@ -1,5 +1,6 @@
 from datetime import timedelta, date
 
+import openpyxl.worksheet.worksheet
 from openpyxl.styles import Border, Side, Alignment, Font
 from openpyxl.styles.borders import BORDER_MEDIUM, BORDER_NONE
 from openpyxl.styles.colors import Color
@@ -22,7 +23,6 @@ no_show_reservation_fill = PatternFill(start_color='FFF39C38', end_color='FFF39C
 RED = Color(rgb='FF0000')
 
 
-
 def fill_first_cell(current_cell, channel, state=''):
     if channel.lower() == 'booking':
         current_cell.fill = booking_fill
@@ -30,8 +30,8 @@ def fill_first_cell(current_cell, channel, state=''):
         current_cell.fill = airbnb_fill
     elif channel.lower() == 'expedia':
         current_cell.fill = expedia_fill
-    elif channel.lower() in ('diretto', 'prolungamento', 'anticipo arrivo', 'kyos', 'ritorno', 'saltato booking',\
-                               'saltato sito', 'sito sahi be', 'subito', 'saltato airbnb', 'saltato expedia'):
+    elif channel.lower() in ('diretto', 'prolungamento', 'anticipo arrivo', 'kyos', 'ritorno', 'saltato booking', \
+                             'saltato sito', 'sito sahi be', 'subito', 'saltato airbnb', 'saltato expedia'):
         current_cell.fill = direct_fill
     if state.lower() == 'no show':
         current_cell.fill = no_show_fill
@@ -45,11 +45,12 @@ def fill_cell(rate, current_cell, state=''):
     if state.lower() == 'no show':
         current_cell.fill = no_show_fill
     elif state.lower() == 'penale':
-        current_cell.fill =penalty_fill
+        current_cell.fill = penalty_fill
 
 
 def start_reservation_border():
-    upper_side_border = Border(top=Side(border_style=BORDER_MEDIUM), left=Side(border_style=BORDER_MEDIUM), right=Side(border_style=BORDER_MEDIUM))
+    upper_side_border = Border(top=Side(border_style=BORDER_MEDIUM), left=Side(border_style=BORDER_MEDIUM),
+                               right=Side(border_style=BORDER_MEDIUM))
     return upper_side_border
 
 
@@ -59,7 +60,8 @@ def middle_reservation_border():
 
 
 def end_reservation_border():
-    bottom_border = Border(bottom=Side(border_style=BORDER_MEDIUM), left=Side(border_style=BORDER_MEDIUM), right=Side(border_style=BORDER_MEDIUM))
+    bottom_border = Border(bottom=Side(border_style=BORDER_MEDIUM), left=Side(border_style=BORDER_MEDIUM),
+                           right=Side(border_style=BORDER_MEDIUM))
     return bottom_border
 
 
@@ -92,7 +94,6 @@ def set_value_occupation_cell(current_cell):
 
 
 def set_rendiconto_occupation_cell(ws_rendiconto, start_row_rendiconto, info_reservation, occupation_column):
-
     date_check_in = info_reservation['Entrata']
     date_check_out = info_reservation['Uscita']
     channel = info_reservation['Tramite']
@@ -113,7 +114,8 @@ def set_rendiconto_occupation_cell(ws_rendiconto, start_row_rendiconto, info_res
         current_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=occupation_column)
 
         while current_date_cell.value != date_check_out:
-            if current_date_cell.value is not None and date_check_in < current_date_cell.value < date_check_out - timedelta(days=1):
+            if current_date_cell.value is not None and date_check_in < current_date_cell.value < date_check_out - timedelta(
+                    days=1):
                 current_cell.border = middle_reservation_border()
                 set_value_occupation_cell(current_cell)
                 fill_cell(rate, current_cell, state)
@@ -134,7 +136,8 @@ def set_rendiconto_occupation_cell(ws_rendiconto, start_row_rendiconto, info_res
         fill_first_cell(current_cell, channel)
 
 
-def set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, gross_column, start_row_rendiconto, offset_date):
+def set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, gross_column, start_row_rendiconto,
+                       offset_date):
     column_letter = get_column_letter(gross_column)
     row_index = start_row_rendiconto + offset_date
 
@@ -151,13 +154,13 @@ def set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, 
                 new_gross_price = ws_rendiconto.cell(row_index, gross_column).value.split('+')[1]
                 type_multiply = current_cell.value.split('*')[1:]
                 for multiplier in type_multiply:
-                    result = result+multiplier+'*'
+                    result = result + multiplier + '*'
                 if len(type_multiply) != 0:
                     current_cell.value = f'={old_gross_price}*{result[:-1]} + {new_gross_price}*0.82'
                 else:
                     current_cell.value = f'={old_gross_price} + {new_gross_price}*0.82'
 
-        if channel.lower() in ('diretto', 'prolungamento', 'anticipo arrivo', 'kyos', 'ritorno', 'saltato booking',\
+        if channel.lower() in ('diretto', 'prolungamento', 'anticipo arrivo', 'kyos', 'ritorno', 'saltato booking', \
                                'saltato sito', 'sito sahi be', 'subito', 'saltato airbnb', 'saltato expedia'):
             if current_cell.value is None:
                 current_cell.value = '=' + column_letter + str(row_index)
@@ -174,7 +177,7 @@ def set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, 
                     current_cell.value = f'={old_gross_price} + {new_gross_price}'
 
         if channel.lower() in ('airbnb'):
-            if  date(2018, 1, 3) <= current_date_cell.value.date() <= date(2018, 7, 14):
+            if date(2018, 1, 3) <= current_date_cell.value.date() <= date(2018, 7, 14):
                 if current_cell.value is None:
                     current_cell.value = f'={column_letter}{row_index} * (1 - 3%*1.22)'
                 elif current_cell.value is not None:
@@ -218,10 +221,8 @@ def set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, 
                         current_cell.value = f'={old_gross_price} + {new_gross_price}*(1-14%*1.22)'
 
 
-
 def set_rendiconto_gross_cell(ws_rendiconto, ws_riepilogo, gross_column, price_column, start_row_rendiconto,
                               reservation, info_reservation):
-
     date_check_in = info_reservation['Entrata']
     date_check_out = info_reservation['Uscita']
     price_per_night = round(info_reservation['Importo notte'], 2)
@@ -266,7 +267,6 @@ def set_rendiconto_gross_cell(ws_rendiconto, ws_riepilogo, gross_column, price_c
 
 
 def set_rendiconto_net_cell(ws_rendiconto, start_row_rendiconto, net_column, gross_column, info_reservation):
-
     date_check_out = info_reservation['Uscita']
     channel = info_reservation['Tramite']
 
@@ -276,7 +276,8 @@ def set_rendiconto_net_cell(ws_rendiconto, start_row_rendiconto, net_column, gro
 
     # imposto il prezzo netto per ogni notte
     while current_date_cell.value != date_check_out:
-        set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, gross_column, start_row_rendiconto, offset_date)
+        set_value_net_cell(ws_rendiconto, current_date_cell, current_cell, channel, gross_column, start_row_rendiconto,
+                           offset_date)
         offset_date += 1
         current_date_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=1)
         current_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=net_column)
@@ -284,7 +285,6 @@ def set_rendiconto_net_cell(ws_rendiconto, start_row_rendiconto, net_column, gro
 
 def set_cell_reservation_break(ws_rendiconto, start_row_rendiconto, info_reservation, taken_column, gross_column,
                                net_column, today, rate):
-
     offset_date = 0
     current_date_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=1)
     while current_date_cell.value is None or current_date_cell.value != today:
@@ -305,10 +305,14 @@ def set_cell_reservation_break(ws_rendiconto, start_row_rendiconto, info_reserva
             gross_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=gross_column)
             net_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=net_column)
             if info_reservation['Giorni'] <= 31:
-                set_value_gross_cell(gross_cell, current_date_cell, info_reservation['Giorno ' + str(info_reservation['Giorni'] - days_remaining + 1)], info_reservation['Tipo tariffa'])
+                set_value_gross_cell(gross_cell, current_date_cell,
+                                     info_reservation['Giorno ' + str(info_reservation['Giorni'] - days_remaining + 1)],
+                                     info_reservation['Tipo tariffa'])
             elif info_reservation['Giorni'] > 31:
-                set_value_gross_cell(gross_cell, current_date_cell, info_reservation['Importo notte'],info_reservation['Tipo tariffa'])
-            set_value_net_cell(ws_rendiconto, current_date_cell, net_cell, info_reservation['Tramite'], gross_column, start_row_rendiconto, offset_date)
+                set_value_gross_cell(gross_cell, current_date_cell, info_reservation['Importo notte'],
+                                     info_reservation['Tipo tariffa'])
+            set_value_net_cell(ws_rendiconto, current_date_cell, net_cell, info_reservation['Tramite'], gross_column,
+                               start_row_rendiconto, offset_date)
             days_remaining -= 1
             offset_date += 1
             current_date_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=1)
@@ -318,7 +322,6 @@ def set_cell_reservation_break(ws_rendiconto, start_row_rendiconto, info_reserva
 
 
 def erase_rendiconto_cell(ws_rendiconto, date_check_out, current_row, taken_column, gross_column, net_column):
-
     current_cell = ws_rendiconto.cell(row=current_row, column=1)
     while ws_rendiconto.cell(row=current_row, column=1).value != date_check_out:
         if current_cell.value is not None:
@@ -339,13 +342,16 @@ def erase_cell(current_cell):
     current_cell.value = None
 
 
-def erase_all_reservation_rendiconto(ws_rendiconto, start_row_rendiconto, end_row_rendiconto):
-
+def erase_all_reservation_rendiconto(ws_rendiconto: openpyxl.worksheet.worksheet.Worksheet, start_row_rendiconto,
+                                     end_row_rendiconto):
     offset_row = 0
+    max_col = ws_rendiconto.max_column
     for index in range(start_row_rendiconto, end_row_rendiconto + 1):
         current_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_row, column=1)
         if current_cell.value is not None:
-            for cell in ws_rendiconto.iter_cols(min_col=3, max_col=75, min_row=start_row_rendiconto + offset_row, max_row=start_row_rendiconto + offset_row):
+            for cell in ws_rendiconto.iter_cols(min_col=3, max_col=max_col,
+                                                min_row=start_row_rendiconto + offset_row,
+                                                max_row=start_row_rendiconto + offset_row):
                 erase_cell(cell[0])
         offset_row += 1
     print('Erased all info in Rendiconto worksheet\n')
@@ -361,6 +367,7 @@ def erase_future_reservation(ws_rendiconto, today, start_row_rendiconto, end_row
     '''
     # initialize some variables
     offset_date = 0
+    max_col = ws_rendiconto.max_column
     current_date_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=1).value
 
     # find today's row
@@ -368,8 +375,7 @@ def erase_future_reservation(ws_rendiconto, today, start_row_rendiconto, end_row
         offset_date += 1
         current_date_cell = ws_rendiconto.cell(row=start_row_rendiconto + offset_date, column=1).value
 
-
-    print('Prima riga da cancellare: ' + str(start_row_rendiconto+offset_date))
+    print('Prima riga da cancellare: ' + str(start_row_rendiconto + offset_date))
 
     # loop from starting row till the end of Rendiconto worksheet
     for index in range(start_row_rendiconto, end_row_rendiconto + 1):
@@ -378,7 +384,8 @@ def erase_future_reservation(ws_rendiconto, today, start_row_rendiconto, end_row
         # if the current date cell is not blank
         if current_cell.value is not None:
             # loop over the column of all apartments
-            for cell in ws_rendiconto.iter_cols(min_col=3, max_col=75, min_row=start_row_rendiconto + offset_date,
+            for cell in ws_rendiconto.iter_cols(min_col=3, max_col=max_col,
+                                                min_row=start_row_rendiconto + offset_date,
                                                 max_row=start_row_rendiconto + offset_date):
                 # delete content of the cell
                 erase_cell(cell[0])
@@ -399,7 +406,6 @@ def stylish_apartment_cell(ws, reservation, starting_row, column_price):
 
 
 def stylish_name_guest_cell(ws, reservation, starting_row, column_price):
-
     rate = reservation['rate'].lower()
     state = reservation['state'].lower()
     name_guest = reservation['name_guest']
@@ -418,7 +424,6 @@ def stylish_name_guest_cell(ws, reservation, starting_row, column_price):
 
 
 def stylish_header_cell(ws, starting_row, column_price):
-
     ws.cell(row=starting_row, column=column_price).value = 'Data'
     ws.cell(row=starting_row, column=column_price).alignment = Alignment(horizontal='center')
     ws.cell(row=starting_row, column=column_price + 1).value = 'Importo'
@@ -432,7 +437,6 @@ def stylish_header_cell(ws, starting_row, column_price):
 
 
 def stylish_channel_cell(ws, reservation, starting_row, column_price):
-
     channel = reservation['channel']
     current_cell = ws.cell(row=starting_row, column=column_price + 2)
     current_cell.value = channel
@@ -442,7 +446,6 @@ def stylish_channel_cell(ws, reservation, starting_row, column_price):
 
 
 def stylish_price_per_night(ws, reservation, starting_row, column_price):
-
     days = reservation['days']
     check_in_year = reservation['check-in'].year
     check_in_month = reservation['check-in'].month
@@ -454,7 +457,7 @@ def stylish_price_per_night(ws, reservation, starting_row, column_price):
             current_price_cell = ws.cell(row=starting_row + offset_date, column=column_price + 1)
 
             current_date_cell.value = date_check_in + timedelta(days=offset_date)
-            current_price_cell.value = reservation[f'price{offset_date+1}']
+            current_price_cell.value = reservation[f'price{offset_date + 1}']
 
             current_date_cell.number_format = '[$-F800]dddd\\,\\ mmmm\\ dd\\,\\ yyyy'
             current_price_cell.style = 'Migliaia'
@@ -473,19 +476,18 @@ def stylish_price_per_night(ws, reservation, starting_row, column_price):
             elif offset_date == 1:
                 current_date_cell.value = date_check_out
 
-            current_price_cell.value = round(total_price/2, ndigits=2)
+            current_price_cell.value = round(total_price / 2, ndigits=2)
             current_price_cell.style = 'Migliaia'
 
             current_date_cell.number_format = '[$-F800]dddd\\,\\ mmmm\\ dd\\,\\ yyyy'
             current_price_cell.style = 'Migliaia'
 
-    starting_row += (offset_date+1)
+    starting_row += (offset_date + 1)
 
     return starting_row
 
 
 def stylish_sum_price(ws, reservation, starting_row, column_price):
-
     days = reservation['days']
     letter_column = get_column_letter(column_price + 1)
 
@@ -494,13 +496,13 @@ def stylish_sum_price(ws, reservation, starting_row, column_price):
     current_cell.alignment = Alignment(horizontal='right')
     current_cell.font = Font(color=RED)
 
-    current_cell = ws.cell(row=starting_row, column=column_price+1)
+    current_cell = ws.cell(row=starting_row, column=column_price + 1)
     if days > 31:
-        current_cell.value = f'=SUM({letter_column}{starting_row-2}:{letter_column}{starting_row-1})'
+        current_cell.value = f'=SUM({letter_column}{starting_row - 2}:{letter_column}{starting_row - 1})'
 
 
     elif days <= 31:
-        current_cell.value = f'=SUM({letter_column}{starting_row-days}:{letter_column}{starting_row-1})'
+        current_cell.value = f'=SUM({letter_column}{starting_row - days}:{letter_column}{starting_row - 1})'
 
     current_cell.style = 'Migliaia'
 
