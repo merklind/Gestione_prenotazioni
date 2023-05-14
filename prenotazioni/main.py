53e431a67baf89620c166719332f20bf6bb2d102
Marco Melis <merklind@libero.it>
2023-05-14 22:45:59 +0200

    Wrap all_reservation and future_reservation into functions

--- prenotazioni/main.py
+++ prenotazioni/main.py
@@ -1,14 +1,14 @@
 from sys import exit
+from main_future_reservation import future_reservation
+from main_all_reservation import all_reservation
 
 opt = int(input(f'Vuoi calcolare:\n1) Solo le prenotazioni future\n2) Tutte le prenotazioni\n\n'))
 
 while opt != (1, 2):
     if opt == 1:
-        import prenotazioni.main_future_reservation
-        exit()
+        future_reservation()
     elif opt == 2:
-        import prenotazioni.main_all_reservation
-        exit()
+        all_reservation()
     else:
         print(f'Hai sbagliato a digitare\n')
         opt = int(input(f'Vuoi calcolare:\n1) Solo le prenotazioni future\n2) Tutte le prenotazioni\n\n'))
